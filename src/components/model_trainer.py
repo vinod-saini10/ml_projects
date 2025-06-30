e608368545eb4b719a60e1cbe0fd11ec009d1639
Vinodsaini <vinodsaini.bit@gmail.com>
2025-06-30 16:19:33 +0530

    DataTransformation done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -97,8 +97,8 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
-                raise CustomException("No best model found")
+            if best_model_score < 0.6:
+                raise CustomException("No best model found", sys)
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
